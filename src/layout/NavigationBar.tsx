d97939c1b1507040dad608306fc7ab4824a0f305
Yuuki <me@june8th.eu.org>
2026-04-09 21:49:53 +0700

    chore(deps): replace @heroicons/react with lucide-react

--- src/layout/NavigationBar.tsx
+++ src/layout/NavigationBar.tsx
@@ -1,8 +1,9 @@
-import { Button } from "@heroui/react";
+import { Button, Drawer } from "@heroui/react";
 import { Logo } from "../components/Branding";
 import LanguageSelector from "../components/LanguageSelector";
-import { ArrowRightEndOnRectangleIcon, Bars3CenterLeftIcon } from "@heroicons/react/24/outline";
 import { useTranslation } from "react-i18next";
+import { LogIn, Menu } from "lucide-react";
+
 
 export function NavigationBar() {
   const { t } = useTranslation();
@@ -13,15 +14,24 @@ export function NavigationBar() {
     <nav className="border-b-2 border-border bg-background/90 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="max-w-400 mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <Button variant="ghost" className="md:hidden">
-            <Bars3CenterLeftIcon />
-            {menuText}
-          </Button>
+          <Drawer>
+            <Button variant="ghost" className="md:hidden">
+              <Menu />
+              {menuText}
+            </Button>
+            <Drawer.Backdrop>
+              <Drawer.Content placement="left">
+                <Drawer.Dialog>
+                  <Drawer.CloseTrigger />
+                </Drawer.Dialog>
+              </Drawer.Content>
+            </Drawer.Backdrop>
+          </Drawer>
           <Logo height={40} />
           <div className="flex items-center gap-2">
             <LanguageSelector />
             <Button>
-              <ArrowRightEndOnRectangleIcon />
+              <LogIn />
               <div className="hidden md:inline">{loginText}</div>
             </Button>
           </div>
