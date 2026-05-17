465b3dc3decc70057eb3a8dd711b20aa97bac04f
nguen <104498646+gawgua@users.noreply.github.com>
2026-05-18 00:53:16 +0700

    feat: Bought tickets listing with QR code

--- src/types/requestDto.ts
+++ src/types/requestDto.ts
@@ -280,3 +280,19 @@ export interface FullUserInfo {
 	createdAt: string;
 	updatedAt: string;
 }
+
+export interface PurchasedTicketView {
+	ticketId: number;
+	purchasedAt: string;
+	purchaseId: number;
+	eventId: number;
+	eventName: string;
+	eventDateTime: string;
+	salesRoundName: string;
+	ticketClassName: string;
+	seatZoneName: string;
+	seatRowLabel: string;
+	seatNumber: number;
+	ticketSecretCode: string;
+	checkedInAt: string | null;
+}
